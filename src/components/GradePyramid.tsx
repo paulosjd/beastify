4b6638f2fb4dc6bdaffb786f08b372e28fd69c24
Paul Davis <paul.davis@hypaship.com>
2023-06-13 18:42:54 +0100

    Logdata charts helper functions and sort data

--- src/components/GradePyramid.tsx
+++ src/components/GradePyramid.tsx
@@ -1,33 +1,26 @@
 import React, { ReactElement, useState } from "react";
-import { Link, IconButton } from "@mui/material";
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
-import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 import styled from "styled-components";
-import Button from "./Button";
-import Input from "./Input";
-import { AppContext, savedItemInterface } from "../AppContext";
-import { Spacing } from "./StyledComponents";
-import TextArea from "./TextArea";
-import styles from "./styles.module.css";
-import ArticleTags from "./ArticleTags";
+import {monthToNumber} from "../helpers";
 
-interface LogItem{
-  grade: string;
-  count: number;
+
+type ChartDataItem = {
+  date: string;
+  uv?: number;
+  pv?: number
 }
 
 interface GradePyramidProps {
-  logData: LogItem[];
+  chartData: ChartDataItem[] | [];
 }
 
-export default function GradePyramid({ logData }: GradePyramidProps): ReactElement {
+export default function GradePyramid({ chartData }: GradePyramidProps): ReactElement {
 
   return (
     <BarChart
       width={500}
       height={300}
-      data={logData}
+      data={chartData}
       margin={{
         top: 5,
         right: 30,
@@ -36,12 +29,12 @@ export default function GradePyramid({ logData }: GradePyramidProps): ReactEleme
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="date" />
       <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="count" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
+      <Bar dataKey="uv" fill="#8884d8" />
+      <Bar dataKey="pv" fill="#8884d8" />
+
+
     </BarChart>
   );
 }
