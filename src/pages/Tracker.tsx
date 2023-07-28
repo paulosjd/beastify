b22c2931d0c578fe9ef2323fc0d61887f251738a
Paul Davis <paul.davis@hypaship.com>
2023-07-28 19:43:17 +0100

    Custom tooltip for tracker chart

--- src/pages/Tracker.tsx
+++ src/pages/Tracker.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement, useState } from "react";
 import useGoogleSheets from "use-google-sheets";
 import CircularProgress from '@mui/material/CircularProgress';
-import { Line, LineChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import { Line, LineChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis, TooltipProps } from "recharts";
+import { ValueType, NameType, } from 'recharts/types/component/DefaultTooltipContent';
 import { getColorForIndex, dateFromTimestamp, linearRegression } from "../lib/helpers";
 import { NoDataParagraph, Wrapper } from "../components/StyledComponents";
 import styled from "styled-components";
@@ -11,12 +12,18 @@ const LogbookWrapper = styled(Wrapper)`
   margin-top: 15px;
 `;
 
+const TooltipWrapper = styled.div`
+  border: 1px solid #717171;
+  background-color: #FFF;
+  padding: 2px 4px 0 4px;
+`;
+
 const Tracker = (): ReactElement => {
+
   const [chartType, setChartType] = useState<string>('absolute');
   const lineColors = ['#1F77B4', '#ff7f0e', '#2ca02c', '#9467bd', '#bcbd22'];
-
+  const dataKeys = ['1r', '2r', '3r', '4r', '5r'];
   const showWeightLine = chartType === 'bodyWeight';
-
   const sheetsObj = {
     apiKey: process.env.REACT_APP_SHEETS_API_KEY || '',
     sheetId: process.env.REACT_APP_PULLUPS_SHEET_ID || '',
@@ -51,11 +58,9 @@ const Tracker = (): ReactElement => {
     .filter(row => ['1', '2', '3', '4', '5'].includes(row.Reps))
     .filter(row => !isNaN(parseFloat(row.Weight)));
 
-  // TODO CustomContentOfTooltip ??  - notes column on spreadsheet and include in tooltip
-
   let chartData: Record<string, string | number>[] = [];
   for (let row of sheetData) {
-    const { Date: date, Weight: weight, Reps: reps } = row;
+    const { Date: date, Weight: weight, Reps: reps, Notes: notes } = row;
     const time = (new Date(date)).getTime();
     if (isNaN(time)) {
       continue;
@@ -63,8 +68,9 @@ const Tracker = (): ReactElement => {
     let dataObj = chartData.find(d => d.time === time);
     if (dataObj) {
       dataObj[`${reps}r`] = weight;
+      dataObj[`${reps}r_notes`] = notes;
     } else {
-      chartData.push({ time, [`${reps}r`]: weight });
+      chartData.push({ time, [`${reps}r_notes`]: notes, [`${reps}r`]: weight });
     }
   }
   chartData = chartData.sort((a, b) => a.time > b.time ? 1 : a.time === b.time ? 0 : -1);
@@ -90,8 +96,6 @@ const Tracker = (): ReactElement => {
     }
   }
 
-  const dataKeys = ['1r', '2r', '3r', '4r', '5r'];
-
   if (showWeightLine) {
     const newChartData: Record<string, string | number>[] = [];
     for (let record of chartData) {
@@ -113,6 +117,7 @@ const Tracker = (): ReactElement => {
             if (dataKeys.includes(key) && typeof value === 'string' && !isNaN(parseFloat(value))) {
               value = ((parseFloat(value) / bodyWeight) * 100).toFixed(2);
               newObj[key] = value;
+              newObj[`${key}_notes`] = record[`${key}_notes`];
             }
           }
           newChartData.push({ ...newObj, bodyWeight: bodyWeight.toFixed(2) });
@@ -121,6 +126,33 @@ const Tracker = (): ReactElement => {
     }
     chartData = newChartData;
   }
+
+  const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
+    if (active && payload && payload.length) {
+      return (
+        <TooltipWrapper className="custom-tooltip">
+          <p className="label" style={{ color: '#717171', margin: 8 }}>{dateFromTimestamp(label)}</p>
+          <div>
+            {payload.filter((pld => pld.dataKey !== 'bodyWeight')).map((pld) => (
+                <div style={{ display: "inline-block", verticalAlign: 'top' }}>
+                  <>
+                    <div style={{ color: pld.stroke, paddingLeft: 10, paddingRight: 10 }}>{`${pld.value} ${chartType === 'bodyWeight' ? '%' : 'kg'}`}</div>
+                    <div style={{ color: pld.stroke, marginTop: 6 }}>
+                      {typeof pld.dataKey === 'string' ? pld.dataKey.replace('r', ' reps') : ''}
+                    </div>
+                    <div style={{ maxWidth: 180 }}>
+                      <p style={{ color: pld.stroke, paddingLeft: 10, paddingRight: 10, marginTop: 8 }}>{pld.payload[`${pld.dataKey}_notes`]}</p>
+                    </div>
+                  </>
+                </div>
+              )
+            )}
+          </div>
+        </TooltipWrapper>
+      );
+    }
+    return null;
+  };
 
   return (
     <LogbookWrapper>
@@ -148,7 +180,7 @@ const Tracker = (): ReactElement => {
           tickFormatter={dateFromTimestamp}
         />
         <YAxis />
-        <Tooltip labelFormatter={dateFromTimestamp} />
+        <Tooltip content={<CustomTooltip />} />
         <Legend />
         {dataKeys.map((item, ind) => (
           <Line
